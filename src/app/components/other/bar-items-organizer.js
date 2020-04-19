121b98231a3d772e01af0759f2c3b92706dbefc6
Frank Batista <nybatista@aol.com>
2020-04-19 06:06:19 -0400

    added bar items oraganizer

---
+++ src/app/components/other/bar-items-organizer.js
@@ -0,0 +1,20 @@
+export class BarItemsOrganizer{
+
+  constructor(props) {
+    console.log("NEEDS ONLY LIST EL")
+
+  }
+
+  static pullDataFromBoundingBox(){
+
+  }
+
+  static getItemsData(){
+
+  }
+
+  static updateItemsData(){
+
+  }
+
+}

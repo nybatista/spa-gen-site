2f103d3f419d4d10d7bd7e4f5adfe771832b5261
Frank Batista <nybatista@aol.com>
2020-04-19 06:13:19 -0400

    updates

---
+++ src/tests/components/bar-item-organizer.test.js
@@ -0,0 +1,11 @@
+import {BarItemsOrganizer} from "components/other/bar-items-organizer";
+
+describe('root test', () => {
+
+  it('should run bar item organizer tests', () => {
+      console.log("BAR ITEM IS ",BarItemsOrganizer)
+    return true;
+
+  });
+
+});

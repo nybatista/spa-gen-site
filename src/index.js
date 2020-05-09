e6150950970ad54e0d506ac8f78967bace80a487
Frank Batista <nybatista@aol.com>
2020-05-09 07:09:15 -0400

    added latest ramda and rxjs to package.json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import SpaGenData from 'data/route-gen.json';
 import {RouteCreatorTraits} from 'traits/route-creator-traits';
 
 
+/*
+*
+* TODO: CREATE A ROUTE CONFIG VALIDATOR -- CHECKS FOR EITHER STRING OR ROUTEPATH:ROUTENAME
+*
+* */
+
+
+/*
+* TODO: CREATE A LOCALSTORAGE CHECK FOR SPYNE CONFIG OR JUST ROUTES CONFIG
+*
+* */
+
 
 
 console.log("WINDOW IS IFRAME ",window.isIframe);
@@ -17,16 +29,47 @@ const css = require('./scss/main.scss');
 
 const spyneApp = new SpyneApp({debug:true});
 
-spyneApp.registerChannel(new NodeListChannel('CHANNEL_NODE_LIST'));
-spyneApp.registerChannel(new ChannelRouteCreator());
-spyneApp.registerChannel(new ChannelFetch('CHANNEL_ROUTEGEN_JSON', {
-    url:SpaGenData,
-    mapFn: RouteCreatorTraits.routeCreator$SetLastItemInObj
-}));
+const initSpyneAppGenerator = ()=> {
 
+  spyneApp.registerChannel(new NodeListChannel('CHANNEL_NODE_LIST'));
+  spyneApp.registerChannel(new ChannelRouteCreator());
+  spyneApp.registerChannel(new ChannelFetch('CHANNEL_ROUTEGEN_JSON', {
+    url: SpaGenData,
+    mapFn: RouteCreatorTraits.routeCreator$SetLastItemInObj
+  }));
 
 //console.log("DATA SPA GEN ",{SpaGenData});
 
-const mainView = new MainView();
-mainView.appendToDom(document.body);
+  const mainView = new MainView();
+  mainView.appendToDom(document.body);
+
+  window.addEventListener('popstate', function (event) {
+    console.log("window has changed MAIN ",{event})
+  });
+
+  window.addEventListener('pushstate', function (event) {
+    console.log("window has changed pushstate MAIN ",{event})
+  });
+
+
+}
+
+const initIframeCode = ()=>{
+  window.addEventListener('popstate', function (event) {
+    console.log("window has changed IFRAME ",{event})
+  });
+
+  window.addEventListener('pushstate', function (event) {
+    console.log("window has changed pushstate IFRAME ",{event})
+  });
+  console.log("init iframe code ");
+}
+
+
+if (window.isIframe===true){
+  initIframeCode();
+} else{
+  initSpyneAppGenerator();
+
+}
 window.R = require('ramda');

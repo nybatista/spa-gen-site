fe2b03e29d41d13156990e74698df94c61a796db
Frank Batista <nybatista@aol.com>
2020-06-08 07:20:29 -0400

    getting ready to transform all

--- src/app/traits/app-data-generator-traits.js
+++ src/app/traits/app-data-generator-traits.js
@@ -1,5 +1,5 @@
 import {SpyneTrait} from 'spyne';
-import {whereEq, path, compose, pick, map, ifElse, nth, reverse, omit, keys, reduceRight, toPairs, equals, merge, is, prop,filter, head} from 'ramda';
+import {whereEq, path, compose, pick, evolve, map, ifElse, nth, reverse, omit, keys, reduceRight, toPairs, equals, merge, is, prop,filter, head} from 'ramda';
 
 export class AppDataGeneratorTraits extends SpyneTrait {
 
@@ -9,20 +9,31 @@ export class AppDataGeneratorTraits extends SpyneTrait {
 
   }
 
-  static appDataGen$CreateDataFromRoutes(configObj=window){
+  static appDataGen$GetMainPropVals(key, data){
+    console.log("KEY AND DATA ", {key, data})
+
+
+
+
+    const transformations = {
+
+
+
+    }
+
+
+    return {};
+
+  }
+
+  static appDataGen$CreateDataFromRoutes(configObj=window, dataSource){
     const routesJson = path(['Spyne', 'config', 'channels', 'ROUTE', 'routes'], configObj);
-    const valIsObj =  compose(is(Object), nth(1))
-    const keyIsRoutePath = compose(equals('routePath'), nth(0))
     const omitProps = omit(['routeName', '^$', '404']);
-
-
     const getRouteName = prop('routeName');
     const getRouteProps = compose(omitProps);
-    const isObject = compose(is(Object), nth(1), toPairs);
 
 
     const mainObj = {};
-
 
     const parseRouteConfig = (routeObj)=>{
       const {routePath} = routeObj;
@@ -30,62 +41,30 @@ export class AppDataGeneratorTraits extends SpyneTrait {
       const routeProps = getRouteProps(routePath);
 
       const generatePropObj = (pair)=>{
-
         const propObj = {
           [routeName]: pair[0]
         }
+
+        const propsObj = AppDataGeneratorTraits.appDataGen$GetMainPropVals(pair[0], dataSource);
+
+
+
         if (is(Object, pair[1])){
           propObj['content'] = parseRouteConfig(pair[1]);
         }
-
-
          return propObj;
-
       }
-
-
       return compose(map(generatePropObj), toPairs)(routeProps);
-
     }
+
+
 
     mainObj['content'] = parseRouteConfig(routesJson);
 
 
-    console.log(JSON.stringify(mainObj));
-    const routeNamesReducer = (arrPair, acc)=>{
-      if (valIsObj(arrPair)){
-        //reduceRouteNamesFn(arrPair[1]);
-      }
-      if (keyIsRoutePath(arrPair)){
-        const propKey = prop('routeName', arrPair[1])
-        const propKeysPairs = compose(toPairs,omitProps)(arrPair[1]);
 
-        const propKeys = map(isObjOrStr, propKeysPairs);
-        //console.log("PROP KEYS ",[propKey, propKeys]);
-        acc.push([propKey, propKeys])
+     return mainObj;
 
-        console.log("ACCUMULATOR ",acc);
-      }
-      return acc;
-    }
-
-
-    const reduceRouteNamesFn = compose(reverse, reduceRight( routeNamesReducer, []), toPairs);
-    const isObjOrStr = (arrPair) => {
-      const isObject = valIsObj(arrPair)
-      let returnVal = [arrPair[0]];
-      if (isObject === true){
-        returnVal =  [arrPair[0], reduceRouteNamesFn(arrPair[1])];
-      }
-
-      //console.log('is obj or str ', isObject,returnVal)
-
-      return returnVal;
-    }
-
-    const routeNamesReducedArr = reduceRouteNamesFn(routesJson);
-
-    return routeNamesReducedArr;
   }
 
 
--- src/tests/traits/dynamic-app-data-traits.test.js
+++ src/tests/traits/dynamic-app-data-traits.test.js
@@ -1,5 +1,6 @@
 import {DynamicAppDataTraits} from 'traits/dynamic-app-data-traits';
 import {AppDataGeneratorTraits} from 'traits/app-data-generator-traits';
+import {DataSource} from '../mocks/app-data-source-mock';
 
 import {Routes} from '../mocks/routes-data-mock';
 
@@ -17,9 +18,8 @@ describe('should get data based on routeProps', () => {
 
   it('should generate data based on routes config', () => {
 
-    let dataFromRoutes = AppDataGeneratorTraits.appDataGen$CreateDataFromRoutes(Routes);
-    //dataFromRoutes = dataFromRoutes.reverse();
-    console.log('data from routes ',JSON.stringify(dataFromRoutes[0][1][2]));
+    let dataFromRoutes = AppDataGeneratorTraits.appDataGen$CreateDataFromRoutes(Routes, DataSource);
+    console.log('data from routes ',JSON.stringify(dataFromRoutes));
     return true;
   })
 

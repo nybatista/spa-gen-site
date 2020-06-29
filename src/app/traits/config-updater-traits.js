b7ececf94dc38d646086f371210ac7c23b4384d4
Frank Batista <nybatista@aol.com>
2020-06-29 04:16:19 -0400

    added config updater

---
+++ src/app/traits/config-updater-traits.js
@@ -0,0 +1,56 @@
+import {SpyneTrait} from 'spyne';
+import {DynamicAppHeaderContentView} from 'components/ui/header/dynamic-app-header-content-view';
+
+export class ConfigUpdaterTraits extends SpyneTrait {
+
+  constructor(context) {
+    let traitPrefix = 'configUpdater$';
+    super(context, traitPrefix);
+
+  }
+
+
+  static configUpdater$HeaderRouteConfigUpdated(e){
+    const {routes, updateConfigNum} = e.props();
+    //console.log("dynamic app header listening to route update ", {routes,e});
+
+    this.appendView(new DynamicAppHeaderContentView({routes, updateConfigNum}), 'header');
+
+
+  }
+
+  static configUpdater$HeaderRouteChangeEvent(e){
+    const {isDeepLink, routeData} = e.props();
+    const {pageId} = routeData;
+
+    if (isDeepLink===true) {
+      const routes = this.dynApp$GetCurrentRouteJson();
+      this.appendView(new DynamicAppHeaderContentView({routes}), 'header');
+    }
+
+
+    //console.log("ROUTE CHANGE DEEPLINK EVENT ",{isDeepLink});
+    const activeSel = `nav > [data-page-id='${pageId}']`;
+    this.props.el$('nav > a').setActiveItem('selected', activeSel);
+
+  }
+
+  static configUpdater$OnPageViewRenderered(){
+    this.addChannel("CHANNEL_DYNAMIC_APP_ROUTE", true);
+
+
+    this.dynPage$AddPageContent(this.props.data.pageId);
+    const {routes} = this.dynPage$CheckToAddSubnavContent();
+
+    this.props.subTopicData = this.dynPage$GetSubTopicData(this.props.data);
+    this.dynPage$CheckToAddSecondaryTopicPage();
+
+
+    this.props.routes=routes;
+    this.initPage();
+
+  }
+
+
+
+}

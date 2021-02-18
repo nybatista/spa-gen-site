6f9119b3eb431bf76bbd3848df4a339a18dac50c
Frank Batista <nybatista@aol.com>
2021-02-18 04:48:51 -0500

    updates

---
+++ src/index-cli.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+
+const fse = require('fs-extra');
+const path = require('path');
+const args = process.argv;
+
+const methodStr = args.length>=3 ? args[2] : 'empty';
+//console.log(method,args.length, args);
+const undefinedFn = async ()=>console.log('params are undefined.');
+const defaultFn = async ()=>console.log('method param is empty.');
+
+
+
+const emptyTempDir = async()=>{
+
+  const dir = './src/tmp/'
+  fse.emptyDir(dir, err => {
+    if (err) return console.error('temp dir not cleared because ',e)
+    console.log('tmp dir cleared successfully!')
+  })
+
+
+
+}
+
+
+const updateBaseApp = async()=>{
+  const onSrcCopiedOver = async()=>{
+    console.log('src has been copied ');
+  }
+
+  const currentDir = process.cwd();
+  const pathObj = path.parse(currentDir);
+  const {dir} = pathObj;
+  const baseAppSrc = path.join(dir, '/base-app/src');
+  const baseAppDest =  path.resolve('./src/base-app/src');
+
+  //console.log("PAThS ",{baseAppSrc, baseAppDest, currentDir, pathObj})
+
+  try {
+    fse.copySync(baseAppSrc, baseAppDest);
+  } catch(e){
+    console.warn('updateBaseApp: ',e)
+  }
+  await onSrcCopiedOver();
+
+}
+
+
+
+
+
+
+
+
+const methodHash = {
+  empty: defaultFn,
+  updateBaseApp: updateBaseApp,
+  emptyTempDir: emptyTempDir,
+}
+
+
+const methodFn = methodHash[methodStr] || undefinedFn;
+
+methodFn();
